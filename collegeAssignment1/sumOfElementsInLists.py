c9fa461384d34185d001eb07868b185a4a0bf43c
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-06 21:33:06 +0530

    calculating sum of all list items

---
+++ collegeAssignment1/sumOfElementsInLists.py
@@ -0,0 +1,18 @@
+numbers=input("Enter the list elements seperated by spaces : ");
+
+number_list=numbers.split(" ");
+
+count=0;
+
+for i in number_list:
+    count=count+1
+
+for i in range(0,count):
+    number_list[i]=int(number_list[i]);
+
+total=0;
+
+for i in range(0,count):
+    total=total+number_list[i];
+
+print(f"The sum of all the list item is {total}");
