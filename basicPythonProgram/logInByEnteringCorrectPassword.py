d800edf600658d020da1133b2bccebc51fbd28ad
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-08 12:53:42 +0530

    check for correct password

---
+++ basicPythonProgram/logInByEnteringCorrectPassword.py
@@ -0,0 +1,9 @@
+password="ved@123";
+
+passwordInput=" ";
+
+print("Username : vedant");
+while(password!=passwordInput):
+    passwordInput=input("Enter your logIn ID password :");
+else:
+    print("Successfully Loged in !");
