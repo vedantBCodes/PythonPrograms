84429c299c62aa08192ed32bdc3263c414ac593f
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-10 20:36:13 +0530

    successfully removed all the duplicates from a list

--- collegeAssignment2/removeDuplicatesFromAList.py
+++ collegeAssignment2/removeDuplicatesFromAList.py
@@ -1,21 +1,10 @@
-# list1=[10,20,30,10,40,20];
-#
-# cnt=0;
-# for i in list1:
-#     cnt=cnt+1;                     #INCOMPLTE PROGRAM
-# for i in range(0,cnt):
-#     for j in range(0,i):
-#         if(list1[i]==list1[j]):
-#             for k in range(i,cnt-1):
-#                 list1[k]=list1[k+1];
-#             i=i-1;
-#             cnt=cnt-1;
-# print(cnt);
-# cnt2=0;
-# for i in list1:
-#     cnt2=cnt2+1;
-# print(cnt2);
-# print(f"List elements after removing duplicates are :");
-# for x in range(0,cnt):
-#     print(list1[x],end=" ");
+list1=[10,20,30,20,30,10,50,60];
+
+emptyList=[];
+for i in list1:
+    if i not in emptyList:
+        emptyList.append(i);
+
+print(f"List elements after removing duplicates are :",end=" ");
+print(emptyList);
 
