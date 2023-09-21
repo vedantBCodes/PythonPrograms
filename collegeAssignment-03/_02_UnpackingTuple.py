5e9fe0c726ddcacdab0ce725f1b5737ade4f0d04
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-21 10:40:34 +0530

    unpacking tuple items

---
+++ collegeAssignment-03/_02_UnpackingTuple.py
@@ -0,0 +1,12 @@
+
+#Unpacking Tuple into several variables
+
+EData=[21,"vedant",70000.00,True];  #EData --> Employee Data
+
+EID=EData[0];
+
+EName=EData[1];
+
+ESalary=EData[2];
+
+EisMale=EData[3];
