3db55ea45623583c4a4528c68b9981b6e78de290
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-22 10:19:02 +0530

    min and max items from a set

---
+++ collegeAssignment-04/_07_Min()AndMax()MethodsFromASet.py
@@ -0,0 +1,11 @@
+#smallest and largest items from a set
+
+set1={34,22,18,41,13};
+
+minItem=min(set1);
+
+print(f"Smallest item from a given set is : {minItem}");
+
+maxItem=max(set1);
+
+print(f"Largest item from a given set is : {maxItem}");
