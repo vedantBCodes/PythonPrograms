b403311cf08701633f9f59c45c0d000e9ff10d30
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-22 09:29:33 +0530

    adding and updating items to set

---
+++ collegeAssignment-04/_02_AddingItemsToSet.py
@@ -0,0 +1,19 @@
+# Adding Items To Sets
+
+set1={1,2,3,4,5};
+
+set1.add(6);
+
+print(set1);
+
+#Updating items to set
+
+set1.update([7,8,9,10]);
+
+print(set1);
+
+set2={11,12,13,14,15};
+
+set1.update(set2);
+
+print(set1);
