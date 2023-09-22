c650ff33fec2a5b2e4b61f241b6a9bfbdce00f3a
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-22 09:38:18 +0530

    removing items from a set

---
+++ collegeAssignment-04/_03_RemovingItemsFromASet.py
@@ -0,0 +1,21 @@
+# Removing items from a given set
+
+set1={1,2,3,4,5}
+
+#Method -1   (Using remove() method)
+
+set1.remove(3);
+
+print(set1);
+
+#Method -2   (Using pop() method)
+
+set1.pop();
+
+print(set1);
+
+#Method -3   (Using clear() method)
+
+set1.clear();
+
+print(set1);
