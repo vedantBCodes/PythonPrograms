d0c6d353e8f2d9519aa6429765a7f2c61d13b383
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-22 09:49:19 +0530

    intersection of  set

---
+++ collegeAssignment-04/_04_IntersectionOfSet.py
@@ -0,0 +1,16 @@
+#Intersection of sets
+
+set1={1,2,3,4,5};
+set2={4,5,6,7,8};
+
+#Method -1    (Using Intersection() Method)
+
+set3=set1.intersection(set2);
+
+print(set3);
+
+#Method -2    (Using & operator)
+
+set4=(set1 & set2);
+
+print(set4);
