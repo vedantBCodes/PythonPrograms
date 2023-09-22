c0ecaa6421d4bfce1738ff3cf6437f6d4ad2d2e5
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-22 09:24:30 +0530

    creating a set

---
+++ collegeAssignment-04/_01_CreatingSet.py
@@ -0,0 +1,21 @@
+#Creating Set
+
+#Method -1
+
+set1=set();      #Defining an empty set
+
+#Method -2
+
+set2={1,2,3,4,5};     #Set with homogeneous items
+set3={21,"vedant",True,80000.00};   #Set with heterogeneous items
+
+#Printing set items
+
+print(set1);
+print(set2);
+print(set3);
+
+#Checking set type
+
+print(type(set1));
+print(type(set3));
