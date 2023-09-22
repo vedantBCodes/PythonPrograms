7a7a02ab7c1d54782bb411e8b1f7376b4d637320
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-22 09:51:11 +0530

    union of  set

---
+++ collegeAssignment-04/_05_UnionOfSet.py
@@ -0,0 +1,16 @@
+#Union of sets
+
+set1={1,2,3,4,5};
+set2={4,5,6,7,8};
+
+#Method -1    (Using Union() Method)
+
+set3=set1.union(set2);
+
+print(set3);
+
+#Method -2    (Using | operator)
+
+set4=(set1 | set2);
+
+print(set4);
