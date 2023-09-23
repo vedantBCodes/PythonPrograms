85d65e8ecf8e70583cdb3e7b0ec1ef02404a6f16
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-23 09:56:59 +0530

    displaying keys

---
+++ collegeAssignment-05/_01_sortingDictionary.py
@@ -0,0 +1,8 @@
+studentID={
+    'vedant':123,
+    'rahul':456,
+    'omkar':789,
+    'aditya':321,
+    'pratik':654
+}
+#Sorting dictionary by keys
---
+++ collegeAssignment-05/_02_PrintingAllKeys.py
@@ -0,0 +1,12 @@
+studentID={
+    'vedant':123,
+    'rahul':456,
+    'omkar':789,
+    'aditya':321,
+    'pratik':654
+}
+#Printing all keys
+
+keys=studentID.keys();
+
+print(keys);
