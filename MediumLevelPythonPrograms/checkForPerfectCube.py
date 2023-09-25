0f814abcc04ea80f2eae0e06184a790214571d49
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-25 15:10:49 +0530

    Checking for perfect cube

--- MediumLevelPythonPrograms/checkForPerfectCube.py
+++ MediumLevelPythonPrograms/checkForPerfectCube.py
@@ -2,14 +2,15 @@ import math
 
 
 def checkForPerfectCube(num):
-    cubeRoot=math.is(num);
+    cubeRoot=round(pow(num,1/3));
     if(pow(cubeRoot,3)==num):
         return True;
     return False;
 
 try:
     num = int(input("Enter a number : "));
-    check = checkForPerfectCube(num);
+    tempNum=abs(num);   #The cube of any negative number is always a negative number
+    check = checkForPerfectCube(tempNum);
 
     if (check == True):
         print(f"{num} is  a perfect cube");
