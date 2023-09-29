10222f4b9365d1283dfd5fa1489eadbbf13564df
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-09-29 18:57:16 +0530

    checking for prime integer

---
+++ PreviousYearPracticalQuestionsTMV/_2023_CheckForPrimeIntegers.py
@@ -0,0 +1,11 @@
+num=int(input("Enter an integer number : "));
+
+check=True;
+for i in range(2,num):
+    if(num%i==0):
+        check=False;
+
+if(check==True and num>1):
+    print(f"{num} is  a prime number");
+else:
+    print(f"{num} is not a prime number");
