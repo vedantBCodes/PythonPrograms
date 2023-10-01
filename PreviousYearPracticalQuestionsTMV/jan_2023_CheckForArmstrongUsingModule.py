6bda6ba93c8055591a8e037a60788a42ca685e29
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-10-01 13:24:56 +0530

    imported a newly created module

---
+++ PreviousYearPracticalQuestionsTMV/jan_2023_CheckForArmstrongUsingModule.py
@@ -0,0 +1,7 @@
+
+
+from moduleToCheckForAnArmstrong import armstrong  #Importing a class from a module
+
+num=int(input("Enter a number :"));
+
+obj=armstrong(num);  # Creating a class which automatically call a constructor
