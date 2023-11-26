4c2f9c610b83d7c74a3aba0d0fcd91da093b64d9
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-11-26 14:59:58 +0530

    Added extends method

--- collegeAssignment-02/appendingAListWithAnotherList.py
+++ collegeAssignment-02/appendingAListWithAnotherList.py
@@ -2,7 +2,17 @@ list1=[10,20,30,40,50];
 
 list2=[60,70,80,90,100];
 
+list3=[11,22,33,44,55];
+
+#Method-1
+
 for i in list2:
     list1.append(i);
-
+    
 print(list1);
+
+#Method-2
+
+list3.extend(list2);
+
+print(list3);
