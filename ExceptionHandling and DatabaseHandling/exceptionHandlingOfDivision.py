00304cf052475219a8f096b9b6fd21e4a4695c71
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-12-02 19:38:28 +0530

    Exception handling of while doing division

---
+++ ExceptionHandling and DatabaseHandling/exceptionHandlingOfDivision.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter a number :"));
+num2=int(input("Enter another number :"));  #Exception occurs if the num2 is zero
+
+result=0;
+
+try:     #Putting all the risky code inside the try block
+    result = num1 / num2;
+
+except:   #Except block will execute if the exception occurs
+    print("can't devide by zero");
+
+else:     #else block will only be executed if the except block doesn't execute
+    print(result);
