7f2b8ed7b2ccc83028e0ead549f763b08231c51a
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-12-09 18:47:23 +0530

    created a class module inside a package

---
+++ packages in python/myPackage/myGreetingClassModule.py
@@ -0,0 +1,12 @@
+class Greeting:
+    def __init__(self):
+        pass;
+
+    def morning(self):
+        print("Hello ! \nGood Morning !\nHave a great day .");
+
+    def night(self):
+        print("Hello !\nGood night !\nHave a sweet dream");
+
+    def afternoon(self):
+        print("Hello !\nGood Evening !\nIt's lovely to see you this afternoon .");
