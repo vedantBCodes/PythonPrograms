196afee5cb8bac1b01c46a25deb6f9e7d59d01fd
vedant Yevatikar <bichkundevedant97@gmail.com>
2023-12-09 18:58:38 +0530

    using class methods by importing class module

---
+++ packages in python/importing class module.py
@@ -0,0 +1,9 @@
+from myPackage import myGreetingClassModule as greet
+
+obj=greet.Greeting(); # Creating object of greeting class
+
+obj.morning();  # Calling class method using an class object
+
+obj.afternoon();
+
+obj.night();
