606e8b7d4b86b37dd66c7789048f749ddcebfea9
vedant Yevatikar <bichkundevedant97@gmail.com>
2024-11-30 19:04:04 +0530

    implemetaion of stack using linked list

---
+++ Algorithms on Stacks/implementationUsingLinkedList.py
@@ -0,0 +1,57 @@
+# Implementation of Stack using Linked list (Dynamic)
+
+# Defining Node
+
+class Node:
+    def __init__(self,data):
+        self.data=data;
+        self.next=None;
+
+class LinkedList:
+    def __init__(self):
+        self.top=None;
+
+    def push(self,data):
+        newNode=Node(data);
+        newNode.next=self.top;
+        self.top=newNode;
+
+    def pop(self):
+        if(self.top==None):
+            print("Stack underflow");
+        else:
+            self.top=self.top.next;
+
+    def peek(self):
+        if (self.top == None):
+            print("Stack is empty");
+        else:
+            print(self.top.data);
+
+    def display(self):
+        if(self.top==None):
+            print("Stack is empty");
+        else:
+            currentNode=self.top;
+            while(currentNode!=None):
+                print(currentNode.data,end=" ");
+                currentNode=currentNode.next;
+
+
+st=LinkedList();
+
+st.push(10);
+st.peek();
+st.push(20);
+st.peek();
+st.push(30);
+st.peek();
+st.push(40);
+st.peek();
+
+st.display();
+print();
+st.pop();
+st.peek();
+
+st.display();
