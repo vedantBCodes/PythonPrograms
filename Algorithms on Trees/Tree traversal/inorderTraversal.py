82858779f1b231c9421309efd31ed56fd471f19c
vedant Yevatikar <bichkundevedant97@gmail.com>
2024-12-03 21:51:32 +0530

    inorder traversal on BST

---
+++ Algorithms on Trees/Tree traversal/inorderTraversal.py
@@ -0,0 +1,57 @@
+#Pre-order Tree traversal
+
+# Creating a node
+
+class Node:
+    def __init__(self,data):
+        self.data=data;
+        self.left=None;
+        self.right=None;
+
+class BST:
+    def __init__(self):
+        self.root=None;
+
+    def insert(self,value):
+        newNode=Node(value);
+        if(self.root==None):
+            self.root=newNode;
+        else:
+            temp=self.root;
+            while(temp!=None):
+                if(value<temp.data):
+                    if(temp.left==None):
+                        temp.left=newNode;
+                        break;
+                    else:
+                        temp=temp.left;
+                else:
+                    if(temp.right==None):
+                        temp.right=newNode;
+                        break;
+                    else:
+                        temp=temp.right;
+
+    def returnRoot(self):
+        return self.root;
+    def inorder(self,root):   # Left --> Root --> Right
+        if(root==None):
+            return;                          #Hint
+        self.inorder(root.left);             #Left
+        print(root.data,end=" ");            #Root
+        self.inorder(root.right);            #Right
+
+obj=BST();
+obj.insert(50);
+obj.insert(10);
+obj.insert(5);
+obj.insert(15);
+obj.insert(20);
+obj.insert(12);
+obj.insert(70);
+obj.insert(55);
+obj.insert(80);
+
+rootNode=obj.returnRoot();
+obj.inorder(rootNode);
+
